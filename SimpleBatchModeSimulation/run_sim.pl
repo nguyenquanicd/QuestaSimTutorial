34e05b60e9a22ba8a78209bc3cad34cfee5cbc0f
nguyenquanicd <nguyenquan.icd@gmail.com>
2020-10-11 10:22:11 +0700

    Update run_sim.pl

--- SimpleBatchModeSimulation/run_sim.pl
+++ SimpleBatchModeSimulation/run_sim.pl
@@ -1,20 +1,3 @@
-#!/strawberry_5_26_0_2/perl/bin
-
-#-----------------------------------------------------------
-# Function:	QuestaSim running script on Windows
-#-----------------------------------------------------------
-# Author  : Nguyen Hung Quan
-# Blog    : http://nguyenquanicd.blogspot.com/
-# Github  : https://github.com/nguyenquanicd
-# LinkIn  : https://www.linkedin.com/company/icdesign-vlsi-technology/
-# Facebook: https://www.facebook.com/integratedcircuitdesign/
-# Twitter : https://twitter.com/NguyenQ23302315?s=03
-# YouTube : https://www.youtube.com/channel/UC0EoSTTMWQqhem-0fXNJKfg
-# email   : nguyenquan.icd@gmail.com
-#-----------------------------------------------------------
-# History:
-#-----------------------------------------------------------
-
 #---------------------------------------------
 #The installed directory of Simulation tool
 #---------------------------------------------
@@ -45,34 +28,33 @@ if ($ARGV[0] eq "-wave") {
   } else {
     system "$VWave $ARGV[1]";
   }
-} elsif ($ARGV[0] eq "-sim") {
+} elsif ($ARGV[0] =~ "-sim") {
   #If the working directory is existed, it's removed
   if (-d "MyWork") { 
     system "rm -rf MyWork";
   }
   
   #Create the working library
+  #C:/questasim64_10.2c/win64/vlib.exe MyWork
   system "$VLib MyWork";
   
   #Compile the source code
-  system "$VLog -work MyWork -sv MooreFsm.sv TestBench.sv -l vlog.log";
+  system "$VLog -work MyWork -sv -f SrcList.f -l vlog.log";
   
   #Optimize the design
   system "$VOpt -work MyWork TestBench -o MyOpt";
-  
-  #Load the design into the simulator
-  #vsim -c MyWork.MyOpt -l vsim.log
-  #vsim -c MyWork.TestBench -l vsim.log
-  #vsim -c -nonvopt MyWork.MyOpt -l vsim.log
-  
-  #system "$VSim -c MyWork.TestBench -do \"run -all; quit\" -l vsim.log";
-  
-  system "$VSim -c -novopt MyWork.TestBench -do \"run -all; quit\" -l vsim.log";
-  
-  #system "$VSim -c -novopt MyWork.MyOpt -do \"run -all; quit\" -l vsim.log";
+
+  #Load the design into the simulator and run
+  if ($ARGV[0] eq "-sim") {
+    system "$VSim -c MyWork.MyOpt -do \"run -all; quit\" -l vsim.log";
+  }
+  else {
+    system "$VSim -c -novopt MyWork.TestBench -do \"run -all; quit\" -l vsim.log";
+  }
 } else {
   print "Please select one of the below options:\n";
-  print " -sim                 : Run the simulation\n";
+  print " -sim                 : Run the simulation with the optimization\n";
+  print " -simnovopt           : Run the simulation without the optimization\n";
   print " -wave <VCD file name>: View the waveform\n";
 }
 
